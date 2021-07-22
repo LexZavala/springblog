11494be105617f6d039a91cad5726a4de11b6705
Lex Zavala <lex.zavala7@gmail.com>
2021-07-22 11:51:56 -0500

    refactoring on walkthrough

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,17 +1,27 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
+    List<Post> posts = new ArrayList<>();
+
+
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String takeToIndex(){
-        return "<h1>This is where posts would be</h1>";
+    public String takeToIndex(Model model){
+        posts.add(new Post("This is post1", "This is post1s body"));
+        posts.add(new Post("This is post2", "This is post2s body"));
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
