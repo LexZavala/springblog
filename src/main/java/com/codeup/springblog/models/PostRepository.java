308c5b353c012191bb179f67efa2f78a5202294d
Lex Zavala <lex.zavala7@gmail.com>
2021-07-25 21:05:54 -0500

    deleted empty space from code

--- src/main/java/com/codeup/springblog/models/PostRepository.java
+++ src/main/java/com/codeup/springblog/models/PostRepository.java
@@ -3,6 +3,4 @@ package com.codeup.springblog.models;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface PostRepository extends JpaRepository <Post, Long> {
-
-}
+public interface PostRepository extends JpaRepository <Post, Long> {}
