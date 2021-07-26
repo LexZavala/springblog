cd1f468114850c6575f4cdc6779c092de351e86b
Lex Zavala <lex.zavala7@gmail.com>
2021-07-26 17:06:25 -0500

    refactored per walkthrough

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -2,6 +2,7 @@ package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
 import com.codeup.springblog.models.PostRepository;
+import com.codeup.springblog.models.User;
 import com.codeup.springblog.models.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -75,14 +76,16 @@ public class PostController {
 //    }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
-    public String createPostForm(){
+    public String createPostForm(Model model){
 
         return "posts/create";
     }
 
-    @RequestMapping(path = "/posts/create/{id}", method = RequestMethod.POST)
-    public String createPost(Model model, @PathVariable long id, @RequestParam(name = "title") String title, @RequestParam(name = "body") String body){
-
-        return "<h1>This is where the post creation gets posted and redirects</h1>";
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    public String createPost(Model model, @RequestParam(name = "title") String title, @RequestParam(name = "body") String body){
+        User user = userDao.getById(1L);
+        Post post = new Post(title, body, user);
+        postDao.save(post);
+        return "redirect:/posts";
     }
 }
