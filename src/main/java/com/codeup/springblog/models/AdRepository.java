11fd1662987141aad954820ce9b38beced3c8494
Lex Zavala <lex.zavala7@gmail.com>
2021-07-26 10:53:10 -0500

    finished lecture on relationships

--- src/main/java/com/codeup/springblog/models/AdRepository.java
+++ src/main/java/com/codeup/springblog/models/AdRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface AdRepository extends JpaRepository<Ad, Long> {
-    Ad findAdById(long id);
+    Ad findById(long id);
 
     @Query("from Ad a WHERE a.title LIKE %:term%")
     Ad findFirstByTitle(String term);
