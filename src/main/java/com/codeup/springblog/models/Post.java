2da2b1a684f0b15d82b1bfdeecd705d035793642
Lex Zavala <lex.zavala7@gmail.com>
2021-07-26 12:06:15 -0500

    added relationship to db between user and post

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -9,27 +9,30 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     @Column(nullable = false, length = 140)
     private String title;
-
     @Column (nullable = false)
     private String body;
-////////
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+
     public Post(){}
 
-    public Post( Long id ,String title, String body) {
-        this.title = title;
-        this.body = body;
+    public Post(long id, String title, String body, User user) {
         this.id = id;
+        this.title = title;
+        this.body = body;
+        this.user = user;
     }
 
-    public Post( String title, String body) {
+    public Post(String title, String body) {
         this.title = title;
         this.body = body;
     }
-
-
 
     public String getTitle() {
         return title;
@@ -53,5 +56,13 @@ public class Post {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
