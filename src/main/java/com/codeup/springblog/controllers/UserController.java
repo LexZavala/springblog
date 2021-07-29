0554d73da7d6e513b3b5d2c10a1a1d9cd1a5f3ca
Lex Zavala <lex.zavala7@gmail.com>
2021-07-29 10:24:25 -0500

    created a UserController

---
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.codeup.springblog.controllers;
+
+import com.codeup.springblog.models.User;
+import com.codeup.springblog.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+    private UserRepository users;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/register";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        users.save(user);
+        return "redirect:/login";
+    }
+}
